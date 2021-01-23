0d934ce43482d49349e8d3dd2198cdb57ae8d05d
Pablo Klaschka <contact@pabloklaschka.de>
2021-01-23 03:05:27 +0100

    refactor: Extract smaller, more readable bits from `DialogContent` component

--- src/components/dialog-content.tsx
+++ src/components/dialog-content.tsx
@@ -1,11 +1,11 @@
-import {h} from 'preact';
-import {useRef, useState} from 'preact/hooks';
+import { h } from 'preact';
+import { Ref, useRef, useState } from 'preact/hooks';
 import SearchResult from './search-result';
-import {useOnChangeCallback} from '../hooks/use-on-change-callback';
-import {Preface} from './preface';
-import {useOnSubmitCallback} from '../hooks/use-on-submit-callback';
-import {Form} from './form';
-import {DialogContentParams} from "./dialog-content-params";
+import { useOnChangeCallback } from '../hooks/use-on-change-callback';
+import { Preface } from './preface';
+import { useOnSubmitCallback } from '../hooks/use-on-submit-callback';
+import { Form } from './form';
+import { DialogContentParams } from './dialog-content-params';
 
 export default function DialogContent(props: DialogContentParams) {
 	const [results, setResults] = useState<
@@ -19,24 +19,46 @@ export default function DialogContent(props: DialogContentParams) {
 	return (
 		<div>
 			<Preface />
-			<Form
-				onSubmit={onSubmit}
-				query={query}
-				onChange={onChange}
-				index={props.index}
-				up={props.up}
-				down={props.down}
-				resultsLength={results.length}
-			/>
-			<ul ref={listRef}>
-				{results.map((result, index) => (
-					<SearchResult
-						key={result.url}
-						active={index === props.index}
-						result={result}
-					/>
-				))}
-			</ul>
+			{getForm(onSubmit, query, onChange, props, results)}
+			{getResultList(listRef, results, props)}
 		</div>
+	);
+}
+
+function getForm(
+	onSubmit: (evt: Event) => void,
+	query: string,
+	onChange: (value: string) => void,
+	props: DialogContentParams,
+	results: { url: string; text: string; title: string }[]
+) {
+	return (
+		<Form
+			onSubmit={onSubmit}
+			query={query}
+			onChange={onChange}
+			index={props.index}
+			up={props.up}
+			down={props.down}
+			resultsLength={results.length}
+		/>
+	);
+}
+
+function getResultList(
+	listRef: Ref<HTMLUListElement>,
+	results: { url: string; text: string; title: string }[],
+	props: DialogContentParams
+) {
+	return (
+		<ul ref={listRef}>
+			{results.map((result, index) => (
+				<SearchResult
+					key={result.url}
+					active={index === props.index}
+					result={result}
+				/>
+			))}
+		</ul>
 	);
 }
