539f618a46a6b734b69e69553b6c0da2b4ced068
Pablo Klaschka <contact@pabloklaschka.de>
2021-01-23 02:56:31 +0100

    refactor: Reduce complexity of `openPage` function

--- src/lib/open-page.tsx
+++ src/lib/open-page.tsx
@@ -1,9 +1,13 @@
+function notOnSamePage(pathname: string) {
+    return (window.location.pathname.endsWith('/')
+        ? window.location.pathname
+        : window.location.pathname + '/') !==
+        (pathname.endsWith('/') ? pathname : pathname + '/');
+}
+
 export function openPage(pathname: string, hash: string) {
 	if (
-		(window.location.pathname.endsWith('/')
-			? window.location.pathname
-			: window.location.pathname + '/') !==
-		(pathname.endsWith('/') ? pathname : pathname + '/')
+		notOnSamePage(pathname)
 	) {
 		window.location.assign(pathname + '#' + hash);
 	} else {
