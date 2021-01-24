894ce68cf17bf9c8d4f478bd433969f7bc6afd79
Pablo Klaschka <contact@pabloklaschka.de>
2021-01-24 14:16:11 +0100

    fix: `openPage` function for Chrome browsers
    Before, scrolling to a new hash on the same page wouldn't work after the great refactoring of be29f76d57e8e54f47e139eff4dc05ae7c27b3f8

--- src/lib/open-page.tsx
+++ src/lib/open-page.tsx
@@ -1,3 +1,29 @@
+function notOnSamePage(pathname: string) {
+	return (
+		(window.location.pathname.endsWith('/')
+			? window.location.pathname
+			: window.location.pathname + '/') !==
+		(pathname.endsWith('/') ? pathname : pathname + '/')
+	);
+}
+
 export function openPage(pathname: string, hash: string) {
-	window.open(pathname + '#' + hash, '_top');
+	if (notOnSamePage(pathname)) {
+		location.assign(pathname + '#' + hash);
+	} else {
+		const element = document.getElementById(hash);
+		if (element) {
+			location.hash = hash;
+			/*
+            Chrome, tested in 87.0.4280.141, doesn't scroll to the element on the same page (with any location function,
+            i.e., location.assign, location.replace, location =, location.href =, ... when scroll-behaviour: smooth
+            is set, e.g., using CSS.
+
+            Also, element.scrollIntoView does nothing (maybe, because it's inside a web component?).
+
+            scrollTo, however, seems to work :P
+             */
+			window.scrollTo({ top: element?.offsetTop || 0 });
+		}
+	}
 }
