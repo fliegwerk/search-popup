822675c1b2ddbd3031d7d84fcd63ab855657a304
Pablo Klaschka <contact@pabloklaschka.de>
2021-01-24 03:40:08 +0100

    fix: `openPage` function

--- src/lib/open-page.tsx
+++ src/lib/open-page.tsx
@@ -1,16 +1,3 @@
-function notOnSamePage(pathname: string) {
-	return (
-		(window.location.pathname.endsWith('/')
-			? window.location.pathname
-			: window.location.pathname + '/') !==
-		(pathname.endsWith('/') ? pathname : pathname + '/')
-	);
-}
-
 export function openPage(pathname: string, hash: string) {
-	if (notOnSamePage(pathname)) {
-		window.location.assign(pathname + '#' + hash);
-	} else {
-		window.location.hash = hash;
-	}
+	window.open(pathname + '#' + hash, '_top');
 }
