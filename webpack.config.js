efcbb188330846685e1fea4fd7a3e83c56b379c2
Pablo Klaschka <contact@pabloklaschka.de>
2021-01-24 03:17:28 +0100

    chore: Fix build and release config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const TerserPlugin = require("terser-webpack-plugin");
-const WebpackLicensePlugin = require("webpack-license-plugin");
+const LicenseWebpackPlugin = require("license-webpack-plugin").LicenseWebpackPlugin;
 const {version, name} = require('./package.json');
 
 module.exports = {
@@ -20,8 +20,8 @@ module.exports = {
         })]
     },
     plugins: [
-        new WebpackLicensePlugin(),
-        new webpack.BannerPlugin({banner: `${name} v${version}. Copyright (c) 2021 by Pablo Klaschka (MIT Licensed). See oss-licenses.json for licenses of open-source projects used and packaged here.`})
+        new LicenseWebpackPlugin(),
+        new webpack.BannerPlugin({banner: `${name} v${version}. Copyright (c) 2021 by Pablo Klaschka (MIT Licensed). See main.licenses.txt for licenses of open-source projects used and packaged here.`})
     ],
     devtool: 'source-map',
     module: {
